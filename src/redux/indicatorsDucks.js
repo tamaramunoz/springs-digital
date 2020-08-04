21039b1943b1462fd689a7b584566043d18d0bbb
tamaramunoz <tami.skt@gmail.com>
2020-08-04 13:30:57 -0400

    creating elements in file indicatorsDucks

---
+++ src/redux/indicatorsDucks.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+// inicial data
+const inicialData = {
+    array: [],
+    value: ''
+}
+
+// actionTypes
+const GET_INDICATOR_SUCCESS = 'GET_INDICATOR_SUCCESS'
+
+// reducer
+
+
+// action creators
