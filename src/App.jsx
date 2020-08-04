bc14506bfd57542b37a2f4c6a9eefb5a9d44617f
tamaramunoz <tami.skt@gmail.com>
2020-08-04 13:44:40 -0400

    navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>hello react</h1>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
+
 const Navbar = () => {
+
     return (
-        <div>
-            Navbar
+        <div className="mt-3">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Financial Indicators</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <button className="btn btn-dark" >Libra</button>
+                        <button className="btn btn-dark" >Desempleo</button>
+                        <button className="btn btn-dark" >Imacec</button>
+                        <button className="btn btn-dark" >Euro</button>
+                        <button className="btn btn-dark" >Dólar Observado</button>
+                        <button className="btn btn-dark" >Dólar Acuerdo</button>
+                        <button className="btn btn-dark" >Unidad Fomento</button>
+                        <button className="btn btn-dark" >Unidad Tributaria</button>
+                        <button className="btn btn-dark" >Bitcoin</button>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
